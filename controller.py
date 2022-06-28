256fea0080ff1532219faca90a102e30ca531d17
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2022-06-28 22:29:36 +0200

    ajout feature pts/joueurs

--- controller.py
+++ controller.py
@@ -29,6 +29,7 @@ def initData(cookie):
 
     for ligue in ligues:
         getItems(ligue, cookie)
+        getPlayers(ligue, cookie)
         getMatchs(ligue, cookie)
         getGamesResult(ligue, cookie)
 
@@ -55,7 +56,7 @@ def getItems(ligue, cookie):
     print('Items ' + ligue + ' importés')
 
 def getPlayers(ligue, cookie):
-    r = requests.get('https://api-' + ligue + '.superfantasylol.com/api/v1/playerinfo', headers={
+    r = requests.get('https://api-' + ligue + '.superfantasylol.com/api/v1/playerinfos', headers={
         'cookie': cookie.replace(u"\u2018", "'").replace(u"\u2019", "'"),
         'origin': 'https://' + ligue + '.superfantasylol.com',
         'referer': 'https://' + ligue + '.superfantasylol.com',
@@ -150,45 +151,97 @@ def getGamesResult(ligue, cookie):
 
             result = json.loads(r.text).get('data')
             if result is not None:
-                top.append(result.get('local').get('players')[TOP].get('items'))
-                top.append(result.get('visitor').get('players')[TOP].get('items'))
+                top.append({
+                    'playerId': result.get('local').get('players')[TOP].get('player').get('nickname'),
+                    'items': result.get('local').get('players')[TOP].get('items')
+                })
+                top.append({
+                    'playerId': result.get('visitor').get('players')[TOP].get('player').get('nickname'),
+                    'items': result.get('visitor').get('players')[TOP].get('items')
+                })
 
-                jungle.append(result.get('local').get('players')[JUNGLE].get('items'))
-                jungle.append(result.get('visitor').get('players')[JUNGLE].get('items'))
+                jungle.append({
+                    'playerId': result.get('local').get('players')[JUNGLE].get('player').get('nickname'),
+                    'items': result.get('local').get('players')[JUNGLE].get('items')
+                })
+                jungle.append({
+                    'playerId': result.get('visitor').get('players')[JUNGLE].get('player').get('nickname'),
+                    'items': result.get('visitor').get('players')[JUNGLE].get('items')
+                })
 
-                mid.append(result.get('local').get('players')[MID].get('items'))
-                mid.append(result.get('visitor').get('players')[MID].get('items'))
+                mid.append({
+                    'playerId': result.get('local').get('players')[MID].get('player').get('nickname'),
+                    'items': result.get('local').get('players')[MID].get('items')
+                })
+                mid.append({
+                    'playerId': result.get('visitor').get('players')[MID].get('player').get('nickname'),
+                    'items': result.get('visitor').get('players')[MID].get('items')
+                })
 
-                adc.append(result.get('local').get('players')[ADC].get('items'))
-                adc.append(result.get('visitor').get('players')[ADC].get('items'))
+                adc.append({
+                    'playerId': result.get('local').get('players')[ADC].get('player').get('nickname'),
+                    'items': result.get('local').get('players')[ADC].get('items')
+                })
+                adc.append({
+                    'playerId': result.get('visitor').get('players')[ADC].get('player').get('nickname'),
+                    'items': result.get('visitor').get('players')[ADC].get('items')
+                })
 
-                supp.append(result.get('local').get('players')[SUPP].get('items'))
-                supp.append(result.get('visitor').get('players')[SUPP].get('items'))
+                supp.append({
+                    'playerId': result.get('local').get('players')[SUPP].get('player').get('nickname'),
+                    'items': result.get('local').get('players')[SUPP].get('items')
+                })
+                supp.append({
+                    'playerId': result.get('visitor').get('players')[SUPP].get('player').get('nickname'),
+                    'items': result.get('visitor').get('players')[SUPP].get('items')
+                })
 
-                coach.append(result.get('local').get('players')[COACH].get('items'))
-                coach.append(result.get('visitor').get('players')[COACH].get('items'))
+                coach.append({
+                    'playerId': result.get('local').get('players')[COACH].get('player').get('nickname'),
+                    'items': result.get('local').get('players')[COACH].get('items')
+                })
+                coach.append({
+                    'playerId': result.get('visitor').get('players')[COACH].get('player').get('nickname'),
+                    'items': result.get('visitor').get('players')[COACH].get('items')
+                })
+
+                # jungle.append(result.get('local').get('players')[JUNGLE].get('items'))
+                # jungle.append(result.get('visitor').get('players')[JUNGLE].get('items'))
+                #
+                # mid.append(result.get('local').get('players')[MID].get('items'))
+                # mid.append(result.get('visitor').get('players')[MID].get('items'))
+                #
+                # adc.append(result.get('local').get('players')[ADC].get('items'))
+                # adc.append(result.get('visitor').get('players')[ADC].get('items'))
+                #
+                # supp.append(result.get('local').get('players')[SUPP].get('items'))
+                # supp.append(result.get('visitor').get('players')[SUPP].get('items'))
+                #
+                # coach.append(result.get('local').get('players')[COACH].get('items'))
+                # coach.append(result.get('visitor').get('players')[COACH].get('items'))
 
             print('Import game ' + id + ' terminé')
     result = {
         'top': top,
-        'jungle': jungle,
+        'jungler': jungle,
         'mid': mid,
         'adc': adc,
-        'supp': supp,
+        'support': supp,
         'coach': coach,
     }
-    with open(ligue + '-itemsResults.json', 'w+') as results:
+    with open(ligue + '-itemsResults2.json', 'w+') as results:
         json.dump(result, results, default=str)
 
-    print('Games results' + ligue + ' importés')
+    print('Games results ' + ligue + ' importés')
 
 
 def getStats(role, ligue, action):
     stats = {}
-
     os.chdir('C:/Users/Quentin/PycharmProjects/fantasy')
-    with open('./' + ligue + '-itemsResults.json') as results:
-        roleResult = json.load(results).get(role)
+    with open('./' + ligue + '-itemsResults2.json') as results:
+        roleResult = []
+        for result in json.load(results).get(role):
+            roleResult.append(result.get('items'))
         # print(roleResult)
     with open('./' + ligue + '-items2.json') as items:
         object = {}
@@ -240,13 +293,84 @@ def getStats(role, ligue, action):
         print(pd.DataFrame(object).T.sort_values('pts moy total', ascending=False))
         print('\n')
     elif action == 'register':
-        os.makedirs('./' + ligue, exist_ok=True)
-        pd.DataFrame(object).T.sort_values('pts moy total', ascending=False).to_csv('./' + ligue + '/' + role + '.csv',
+        os.chdir('C:/Users/Quentin/PycharmProjects/fantasy/data')
+        os.makedirs('./' + ligue + '/' + role, exist_ok=True)
+        pd.DataFrame(object).T.sort_values('pts moy total', ascending=False).to_csv('./' + ligue + '/' + role + '/all.csv',
                                                                                     index=True)
+def getStatsByPlayer(role, ligue):
+    stats = {}
 
+    os.chdir('C:/Users/Quentin/PycharmProjects/fantasy')
+    with open(ligue + '-players.json') as players:
+        playersData = json.load(players)
+        playersByRole = []
+        for player in playersData:
+            if player.get('role') == role.upper():
+                playersByRole.append(player)
+
+        for player in playersByRole:
+            playerNickname = player.get('nickname')
+            # print(playerNickname)
+
+            os.chdir('C:/Users/Quentin/PycharmProjects/fantasy')
+            with open('./' + ligue + '-itemsResults2.json') as results:
+                roleResult = []
+                for result in json.load(results).get(role):
+                    if result.get('playerId') == playerNickname:
+                        roleResult.append(result.get('items'))
+                # print(roleResult)
+            with open('./' + ligue + '-items2.json') as items:
+                object = {}
+                itemsData = json.load(items)
+                for item in itemsData:
+                    itemId = item.get('id')
+                    name = ''
+                    for translation in item.get('translations'):
+                        if translation.get('locale') == 'fr':
+                            name = translation.get('name')
+                        elif translation.get('locale') == 'en':
+                            en_name = translation.get('name')
+                    isValid = sum(x.get(itemId) is not None and x.get(itemId) > 0 for x in roleResult)
+                    if isValid > 0:
+                        object[name] = {}
+                        object[name]['Nom Anglais'] = en_name
+                        object[name]["nb games valide"] = isValid
+                        object[name]["total games"] = len(roleResult)
+                        object[name]["ratio"] = round(sum(x.get(itemId) is not None and x.get(itemId) > 0 for x in roleResult) / len(
+                            roleResult), 2)
+                        object[name]["%"] = "{:.0%}".format(
+                            sum(x.get(itemId) is not None and x.get(itemId) > 0 for x in roleResult) / len(roleResult))
+                        total = 0
+                        for x in roleResult:
+                            if x.get(itemId) is not None and x.get(itemId) > 0:
+                                total += x.get(itemId)
+                        if sum(x.get(itemId) is not None and x.get(itemId) > 0 for x in roleResult) > 0:
+                            object[name]["pts moy"] = round(total / sum(
+                                x.get(itemId) is not None and x.get(itemId) > 0 for x in roleResult), 2)
+                        else:
+                            object[name]["pts moy"] = 0
+
+                        object[name]["pts moy total"] = round(total / len(roleResult), 2)
+                        object[name]["total point"] = total
+                        object[name]["cout"] = item.get('baseCost')
+                        if object[name]["cout"] > 0:
+                            object[name]["pts par cout moy"] = round(object[name]["pts moy"] / object[name]["cout"], 2)
+                        else:
+                            object[name]["pts par cout moy"] = 'Gratuit'
+
+                        if object[name]["cout"] > 0:
+                            object[name]["pts par cout total"] = round(object[name]["pts moy total"] / object[name]["cout"], 2)
+                        else:
+                            object[name]["pts par cout total"] = 'Gratuit'
+
+
+            os.chdir('C:/Users/Quentin/PycharmProjects/fantasy/data')
+            os.makedirs('./' + ligue + '/' + role, exist_ok=True)
+            pd.DataFrame(object).T.sort_values('pts moy total', ascending=False).to_csv('./' + ligue + '/' + role + '/' + playerNickname + '.csv',
+                                                                                        index=True)
 
 def zip_files(ligue):
-    os.chdir('C:/Users/Quentin/PycharmProjects/fantasy')
+    os.chdir('C:/Users/Quentin/PycharmProjects/fantasy/data')
     shutil.make_archive(ligue, 'zip', './' + ligue)
     print('Zip créé')
     shutil.move(ligue + '.zip', './' + ligue + '/' + ligue + '.zip')
@@ -254,7 +378,7 @@ def zip_files(ligue):
 
 def release():
     tag = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    os.chdir('C:/Users/Quentin/PycharmProjects/fantasy')
+    os.chdir('C:/Users/Quentin/PycharmProjects/fantasy/data')
     os.system('git add .')
     os.system('git commit -m "release"')
     os.system('git pull')
